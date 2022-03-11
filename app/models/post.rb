f2bbfa25ffa379d575b490d3b1fba401fe24a0a7
OsamuHaketa <nrzlt.xtjn@gmail.com>
2022-03-11 07:02:57 +0000

    コメント機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_comments
+
   has_one_attached :post_image
-  
+
   def get_post_image
     (post_image.attached?) ? post_image : 'no_image.png'
   end
