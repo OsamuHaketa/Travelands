4a3ac7dc5604274788effdd55e213197d5087c8b
OsamuHaketa <nrzlt.xtjn@gmail.com>
2022-03-29 05:04:26 +0000

    ページネーション追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: :desc).limit(8)
-    @tag_list = Tag.all
+    @posts = Post.all.order(created_at: :desc).page(params[:page]).per(5)
+    @tag_list = Tag.all.sort {|a,b| b.posts.size <=> a.posts.size}
   end
 end
